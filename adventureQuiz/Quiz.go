5faaac34bbb30b9cde5e011bb0e39aa9b1715f32
johnkinuthiaa <kinuthiajohn122@gmail.com>
2025-01-02 22:35:39 +0300

    trivia

---
+++ adventureQuiz/Quiz.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	scores := 0
+	userAnswer := ""
+	gameOver := false
+	type questionStruct struct {
+		question string
+		answer   string
+	}
+	for gameOver == false {
+		q1 := questionStruct{question: "what is the capital city of kenya? ", answer: "Nairobi"}
+		fmt.Println(q1.question)
+
+		fmt.Scan(&userAnswer)
+		if strings.ToLower(userAnswer) == strings.ToLower(q1.answer) {
+			scores += 1
+			userAnswer = ""
+		} else {
+			gameOver = true
+		}
+		fmt.Println(scores)
+	}
+
+}
